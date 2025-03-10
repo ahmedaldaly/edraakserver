91857792ed2c7bb6edc6f29625cbe6720d7b9635
Ahmed Eldaly <96292450+ahmedaldaly@users.noreply.github.com>
2025-03-10 11:28:35 -0400

    Update auth.js

--- router/auth.js
+++ router/auth.js
@@ -1,5 +1,11 @@
 const router = require('express').Router()
-const {Register,login} = require('../controller/authController')
+const {Register,login,googleAuth, googleCallback, logout} = require('../controller/authController')
 router.post('/register',Register)
 router.post('/login',login)
+
+router.get("/google", googleAuth);
+router.get("/google/callback", googleCallback);
+
+// تسجيل خروج
+router.get("/logout", logout);
 module.exports=router;
